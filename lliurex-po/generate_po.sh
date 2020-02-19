395570ddb3b061146e1e5e2cd8ebce6c390e556b
jrpelegrina <juapesai@hotmail.com>
2020-02-19 16:30:45 +0100

    Changes to support minimal-client

---
+++ lliurex-po/generate_po.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xgettext --join-existing ../install-files/usr/share/lliurex-flavours-selector/lliurex-flavours-selector.py -o ./zero-lliurex-flavours/zero-lliurex-flavours.pot
+xgettext --join-existing ../install-files/usr/share/lliurex-flavours-selector/rsrc/lliurex-flavours-selector.ui  -o ./zero-lliurex-flavours/zero-lliurex-flavours.pot
