50d4c64f0fb9e662f839fb7707968b12f79f1a76
jrpelegrina <juapesai@hotmail.com>
2021-06-30 13:13:56 +0200

    Fixed bug when running in desktop with client meta

--- lliurex-flavours-selector/python3-lliurexflavourselector/flavourSelectorManager.py
+++ lliurex-flavours-selector/python3-lliurexflavourselector/flavourSelectorManager.py
@@ -258,11 +258,12 @@ class flavourSelectorManager:
 
 		for item in self.flavour_list:
 			if self.desktop or self.desktop_lite or self.music or self.infantil or self.empty or self.pime:
-				if self.flavour_list[item]["pkg"] in self.client_meta_available and self.flavour_list[item]["pkg"] not in self.flavours_installed:
-					tmp=[]
-					tmp.append(self.flavour_list[item]["name"])
-					tmp.append(self.flavour_list[item]["pkg"])
-					self.client_desktop_alternatives.append(tmp)
+				if not self.client and not self.client_lite and not self.minimal_client:
+					if self.flavour_list[item]["pkg"] in self.client_meta_available and self.flavour_list[item]["pkg"] not in self.flavours_installed:
+						tmp=[]
+						tmp.append(self.flavour_list[item]["name"])
+						tmp.append(self.flavour_list[item]["pkg"])
+						self.client_desktop_alternatives.append(tmp)
 
 				if self.flavour_list[item]["pkg"] in self.server_meta_available and self.flavour_list[item]["pkg"] not in self.flavours_installed:
 					tmp=[]
